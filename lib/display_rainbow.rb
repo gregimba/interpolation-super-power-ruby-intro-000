037f4cbc1ef0811b8e2c7b567c3c9f72e888af77
Grant Regimbal <regimbal.grant@gmail.com>
2017-08-18 21:26:03 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors.each { |color|  }
-    puts color
+  R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n
 end
